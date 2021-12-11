2cc767fc8906c03821523be690a351a70db24c8a
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-11 14:47:12 +0530

    ADDED:starting position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,6 @@
 public class SnakeLadder {
 	public static void main(String []args){
-
-		System.out.println("Welcome to snake ladder simulator");
+		int start = 0;
+		System.out.println("Starting position of player is: " + start);
 	}
 }
