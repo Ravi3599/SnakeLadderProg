1d0ef08286ba9da48c5a4e4b521e8ae26b47fe83
Ravi3599 <ravikantdukre2018@gmail.com>
2021-12-11 15:06:12 +0530

    UPDATED:no. of times dice played to win

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,16 +6,18 @@ public class SnakeLadder {
 
 
    public static void main(String[] args){
-      int pos = 0;
+      int pos = 0, cntr1 = 0, cntr2 = 0, cntr3 = 0, cntr4 = 0;
       System.out.println("Starting position of player is: " + START);
       while(pos < 100){
       int roll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		cntr1++;
       System.out.println("Number on rolled dice is : " +roll);
       int con = (int) (Math.floor(Math.random() * 10) % 3);
       System.out.println("Condition: " +con);
       switch (con){
             case NO_PLAY:
                   System.out.println("New Position is: " + pos);
+						cntr4++;
                   break;
             case LADDER:
                   pos = pos + roll;
@@ -25,6 +27,7 @@ public class SnakeLadder {
 						pos = pos - roll;
                   System.out.println("Remains at same position: " + pos);
 						}
+						cntr2++;
                   break;
             case SNAKE:
                   pos = pos - roll;
@@ -40,6 +43,7 @@ public class SnakeLadder {
                   else{
                   System.out.println("New position is:" + pos);
 						}
+						cntr3++;
                   break;
             default:
                   System.out.println("Default");
@@ -47,6 +51,7 @@ public class SnakeLadder {
          }
       }
       System.out.println();
+		System.out.println("Player ROLLED DICE "+cntr1+" time and got SNAKE "+cntr3+" times and got LADDER "+cntr2+" times and got NO PLAY "+cntr4+" times to win the game");
       if (pos == 100)
       System.out.println("-------------------");
       System.out.println("    PLAYER WON");
